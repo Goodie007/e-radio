a37aff1e54d33608f73f2603866d1db69603cb65
Aruoture Goodness <aruoturegoodness@gmail.com>
2023-05-27 16:00:50 +0100

    fixes

--- components/config/store.ts
+++ components/config/store.ts
@@ -1,8 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { createWrapper } from "next-redux-wrapper";
+import authReducer from '../auth/authSlice';
 
 export const store =  configureStore({
-    reducer: {}
+    reducer: {
+        auth: authReducer
+    }
 })
 
 const makeStore = () => store;
