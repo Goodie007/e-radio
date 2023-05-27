70abb0c992cef89145496e5d0d5882bcfdf7c2db
Aruoture Goodness <aruoturegoodness@gmail.com>
2023-05-27 22:00:11 +0100

    redux

---
+++ components/modules/authAction.ts
@@ -0,0 +1,33 @@
+import axios from "axios";
+import { createAsyncThunk } from "@reduxjs/toolkit";
+
+
+const supabase = process.env.API_BASE_URL;
+
+export const registerUser = createAsyncThunk(
+    'auth/register',
+    async ( name, password , { rejectWithValue }:type) => {
+        try {
+            const config = {
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+            }
+            await axios.post(
+                `${supabase}/rest/v1/users`,
+                { name, password },
+                config
+            )
+        } 
+        catch (error:any) {
+            // return custom error message from backend if present
+            if (error.response && error.response.data.message) {
+                return rejectWithValue(error.response.data.message)
+            } else {
+                return rejectWithValue(error.message)
+            }
+        }
+    }
+
+)
+
